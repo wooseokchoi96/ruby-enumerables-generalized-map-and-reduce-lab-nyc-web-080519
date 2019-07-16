8bc0eb0b2fa828c98c74395b47b5dba10298e2e9
Andy Choi <wooseokchoi96@gmail.com>
2019-07-16 14:07:07 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,14 @@
-# Your Code Here
+def my own map(array)
+  new=[]
+  array.length.times{ |element|
+    new.push(element)
+    yield new[-1]
+  }
+end
+
+def my own reduce(array,start=0)
+  array.length.times{ |element|
+    start+=element
+    yield start
+  }
+end
